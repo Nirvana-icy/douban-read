420eaba71926a45062c3e0a0647f2b8ddec897ac
jacky li <veryfaint@gmail.com>
2013-01-24 15:20:29 +0800

    Revert "remove everything"
    This reverts commit 49fce0925a47dba37208901d94839c0ab9905a3d.

---
+++ doubanreadTests/doubanreadTests.h
@@ -0,0 +1,13 @@
+//
+//  doubanreadTests.h
+//  doubanreadTests
+//
+//  Created by jli on 1/24/13.
+//  Copyright (c) 2013 jli. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface doubanreadTests : SenTestCase
+
+@end
