55a9afb51b876b0ba84784ff2783af8f86baa5d1
jacky li <veryfaint@gmail.com>
2013-03-12 17:43:17 +0800

    update icon, refactoring

--- doubanread/View/BookCell.m
+++ doubanread/View/BookCell.m
@@ -39,21 +39,29 @@
 }
 
 - (void)redrawImage:(UIImage *)bookImage {
-    CGFloat imageWidth = bookImage.size.width;
-    CGFloat imageHeight = bookImage.size.height;
-    CGFloat cropXStartPoint = 0.0f;
-    CGFloat cropYStartPoint = 0.0f;
-    if (imageWidth > IMAGE_MAX_WIDTH) {
-        cropXStartPoint = (imageWidth - IMAGE_MAX_WIDTH) / 2;
-    }
-    if (imageHeight > IMAGE_MAX_HEIGHT) {
-        cropYStartPoint = (imageHeight - IMAGE_MAX_HEIGHT) / 2;
-    }
-    CGRect rect = CGRectMake(cropXStartPoint, cropYStartPoint, IMAGE_MAX_WIDTH, IMAGE_MAX_HEIGHT);
-    CGImageRef imageRef = CGImageCreateWithImageInRect([bookImage CGImage], rect);
-
-    UIImage *cropped = [UIImage imageWithCGImage:imageRef];
+    UIImage *cropped = [self cropImage:bookImage toWidth:IMAGE_MAX_WIDTH andHeight:IMAGE_MAX_HEIGHT];
     [iconView setImage:cropped];
     [iconView setFrame:CGRectMake(0, 0, cropped.size.width, cropped.size.height)];
+}
+
+- (UIImage *)cropImage:(UIImage *)image toWidth:(int)width andHeight:(int)height {
+    CGFloat imageWidth = image.size.width;
+    CGFloat imageHeight = image.size.height;
+    CGFloat cropXStartPoint = 0.0f;
+    CGFloat cropYStartPoint = 0.0f;
+    if (imageWidth > width) {
+        cropXStartPoint = (imageWidth - width) / 2;
+    }
+    if (imageHeight > height) {
+        cropYStartPoint = (imageHeight - height) / 2;
+    }
+    CGRect rect = CGRectMake(cropXStartPoint, cropYStartPoint, width, height);
+    CGImageRef imageRef = CGImageCreateWithImageInRect([image CGImage], rect);
+
+    UIImage *cropped = [UIImage imageWithCGImage:imageRef];
+    if(imageRef){
+        CFRelease(imageRef);
+    }
+    return cropped;
 }
 @end
