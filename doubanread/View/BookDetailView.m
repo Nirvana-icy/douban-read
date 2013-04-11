bec66d44dc596d130dcdd936a43819a1fb68e2a7
jacky li <veryfaint@gmail.com>
2013-04-11 13:42:35 +0800

    add rating label

--- doubanread/View/BookDetailView.m
+++ doubanread/View/BookDetailView.m
@@ -22,26 +22,34 @@
 
 - (void)createSubviews {
     iconView = [[UIImageView alloc] init];
-    [iconView setFrame:CGRectMake(5, 10, IMAGE_MAX_WIDTH, IMAGE_MAX_HEIGHT)];
+    [iconView setFrame:CGRectMake(10, 10, IMAGE_MAX_WIDTH, IMAGE_MAX_HEIGHT)];
     [iconView setImage:book.mediumImage];
     [self addSubview:iconView];
 
     float height = 10;
-    UILabel *bookNameLabel = [[UILabel alloc] initWithText:[book title] andPosition:CGPointMake(120, height) andMaxWidth:200 fontSize:12.0f];
+    UILabel *bookNameLabel = [self createLabelOnTheRightSizeOfImage:height text:[book title]];
     [self addSubview:bookNameLabel];
     height += bookNameLabel.bounds.size.height + 10;
 
-    UILabel *authorLabel = [[UILabel alloc] initWithText:[NSString stringWithFormat:@"作者: %@", [book author]] andPosition:CGPointMake(120, height) andMaxWidth:200 fontSize:12.0f];
+    UILabel *authorLabel = [self createLabelOnTheRightSizeOfImage:height text:[NSString stringWithFormat:@"作者: %@", [book author]]];
     [self addSubview:authorLabel];
     height += authorLabel.bounds.size.height + 10;
 
     if (![[book publisher] isEqual:@""]) {
-        UILabel *publisherLabel = [[UILabel alloc] initWithText:[NSString stringWithFormat:@"出版社: %@", [book publisher]] andPosition:CGPointMake(120, height) andMaxWidth:200 fontSize:12.0f];
-        [publisherLabel setText:[NSString stringWithFormat:@"出版社: %@", [book publisher]]];
+        UILabel *publisherLabel = [self createLabelOnTheRightSizeOfImage:height text:[NSString stringWithFormat:@"出版社: %@", [book publisher]]];
         [self addSubview:publisherLabel];
+        height += publisherLabel.bounds.size.height + 10;
     }
+    
+    UILabel *rateLabel = [self createLabelOnTheRightSizeOfImage:height text:[NSString stringWithFormat:@"评分: %@ / %@人评价", [book rating],[book numberOfRaters]]];
+    [self addSubview:rateLabel];
+    height += rateLabel.bounds.size.height + 10;
 
     [self addButtonsWithPositionY:IMAGE_MAX_HEIGHT + 20];
+}
+
+- (UILabel *)createLabelOnTheRightSizeOfImage:(float)height text:(NSString *)text {
+    return [[UILabel alloc] initWithText:text andPosition:CGPointMake(130, height) andMaxWidth:200 fontSize:12.0f];
 }
 
 - (void)addButtonsWithPositionY:(float)positionY {
@@ -51,6 +59,6 @@
 - (void)showImage:(UIImage *)image {
     UIImage *cropped = [image cropToWidth:IMAGE_MAX_WIDTH andHeight:IMAGE_MAX_HEIGHT];
     [iconView setImage:cropped];
-    [iconView setFrame:CGRectMake(5, 10, cropped.size.width, cropped.size.height)];
+    [iconView setFrame:CGRectMake(10, 10, cropped.size.width, cropped.size.height)];
 }
 @end
