4165e9713710fc73590795cd8439876423b83ffd
jacky li <veryfaint@gmail.com>
2013-05-01 14:52:31 +0800

    change the star location and size

--- doubanread/View/CommentStarView.m
+++ doubanread/View/CommentStarView.m
@@ -15,7 +15,7 @@
 - (void)createStarButtons {
     [self setFrame:CGRectMake(110, 10, 100, 40)];
     for (int i = 0; i < 5; i++) {
-        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(i * 18 + 10, 11, 18, 18)];
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(i * 20 + 10, 10, 20, 20)];
         [button setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
         [self addSubview:button];
     }
