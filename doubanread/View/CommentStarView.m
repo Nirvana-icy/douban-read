cf667113e85fe80a0f742d7eb19c7457671e3ca6
jacky li <veryfaint@gmail.com>
2013-05-01 16:13:58 +0800

    show the same image when button highlighted

--- doubanread/View/CommentStarView.m
+++ doubanread/View/CommentStarView.m
@@ -16,7 +16,9 @@
 - (void)setBook:(DOUBook *)book {
     for (int i = 0; i < 5; i++) {
         UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(i * 20 + 10, 10, 20, 20)];
-        [button setBackgroundImage:[UIImage imageNamed:[StarUtil getStarName:(NSUInteger) i ratingStars:[book myRatingStars]]] forState:UIControlStateNormal];
+        UIImage *image = [UIImage imageNamed:[StarUtil getStarName:(NSUInteger) i ratingStars:[book myRatingStars]]];
+        [button setBackgroundImage:image forState:UIControlStateNormal];
+        [button setBackgroundImage:image forState:UIControlStateHighlighted];
         [self addSubview:button];
     }
 }
