bb431e81167fb30ea6798b49cb7a1b2ba1b54c38
jacky li <veryfaint@gmail.com>
2013-05-02 22:29:39 +0800

    introduce OCHamcrest

--- doubanreadTests/DouBookTest.h
+++ doubanreadTests/DouBookTest.h
@@ -1,13 +1,5 @@
-//
-//  DouBookTest.h
-//  doubanread
-//
-//  Created by jli on 4/27/13.
-//  Copyright (c) 2013 jli. All rights reserved.
-//
-
 #import <SenTestingKit/SenTestingKit.h>
 
-@interface DouBookTest : SenTestCase
+@interface DOUBookTest : SenTestCase
 
 @end
--- doubanreadTests/DouBookTest.m
+++ doubanreadTests/DouBookTest.m
@@ -1,71 +1,80 @@
-#import "DouBookTest.h"
+#import "DOUBookTest.h"
 #import "DOUBook.h"
 
-@implementation DouBookTest
+#define HC_SHORTHAND
+
+#import "OCHamcrestIOS/OCHamcrestIOS.h"
+
+@implementation DOUBookTest
 
 - (void)test_should_get_stars_when_rating_is_0 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"0"}}}];
     NSArray *rating = @[@0, @0, @0, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_below_1 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"0.9"}}}];
     NSArray *rating = @[@1, @0, @0, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_1_and_2 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"2"}}}];
     NSArray *rating = @[@2, @0, @0, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_2_and_3 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"3"}}}];
     NSArray *rating = @[@2, @1, @0, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_3_and_4 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"4"}}}];
     NSArray *rating = @[@2, @2, @0, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_4_and_5 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"5"}}}];
     NSArray *rating = @[@2, @2, @1, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_5_and_6 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"6"}}}];
     NSArray *rating = @[@2, @2, @2, @0, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_6_and_7 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"7"}}}];
     NSArray *rating = @[@2, @2, @2, @1, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_7_and_8 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"8"}}}];
     NSArray *rating = @[@2, @2, @2, @2, @0];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_8_and_9 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"9"}}}];
     NSArray *rating = @[@2, @2, @2, @2, @1];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
 }
 
 - (void)test_should_get_stars_when_rating_between_9_and_10 {
     DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"book" : @{@"rating" : @{@"average" : @"10"}}}];
     NSArray *rating = @[@2, @2, @2, @2, @2];
-    STAssertEqualObjects([book ratingStars], rating, nil, nil);
+    assertThat([book ratingStars], is(rating));
+}
+
+- (void)test_should_get_my_stars_when_my_rating_is_0 {
+    DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"rating" : @{@"value" : @"0"}}];
+
 }
 @end
