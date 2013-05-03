e1a3db7b2780e6425c3a412845c301c05fd69ba2
jacky li <veryfaint@gmail.com>
2013-05-03 16:32:59 +0800

    click on one star will change other star's status

--- doubanread/View/CommentStarView.m
+++ doubanread/View/CommentStarView.m
@@ -9,6 +9,7 @@
     self = [super initWithCoder:coder];
     if (self) {
         [self setFrame:CGRectMake(110, 10, 100, 40)];
+        starButtons = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -19,7 +20,23 @@
         UIImage *image = [UIImage imageNamed:[StarUtil getStarName:(NSUInteger) i ratingStars:[book myRatingStars]]];
         [button setBackgroundImage:image forState:UIControlStateNormal];
         [button setBackgroundImage:image forState:UIControlStateHighlighted];
+        [button addTarget:self action:@selector(changeStarStatus:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:button];
+        [starButtons addObject:button];
+    }
+}
+
+- (void)changeStarStatus:(id)sender {
+    NSUInteger indexOfObject = [starButtons indexOfObject:sender];
+    for (NSUInteger i = 0; i <= indexOfObject; i++) {
+        UIButton *button = starButtons[i];
+        [button setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateHighlighted];
+    }
+    for(NSUInteger i = indexOfObject+1; i < [starButtons count]; i ++){
+        UIButton *button = starButtons[i];
+        [button setBackgroundImage:[UIImage imageNamed:@"star_none.png"] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:@"star_none.png"] forState:UIControlStateHighlighted];
     }
 }
 
