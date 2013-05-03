7278b2f2ae1f71b5e0087864337c766df3bb772e
jacky li <veryfaint@gmail.com>
2013-05-03 22:52:50 +0800

    extract field

--- doubanread/Model/BookStatusChangeRequest.m
+++ doubanread/Model/BookStatusChangeRequest.m
@@ -6,45 +6,45 @@
 
 @implementation BookStatusChangeRequest {
     id delegate;
+
+    void (^callBackFunction)(DOUHttpRequest *);
 }
 - (id)initWithDelegate:(id)aDelegate {
     self = [super init];
     if (self) {
         delegate = aDelegate;
+        [self initCallBackFunction];
     }
     return self;
 }
 
-- (void)updateBook:(DOUBook *)book{
+- (void)updateBook:(DOUBook *)book {
     [self doSomethingWithBook:book withBlock:^(DOUQuery *query, DOUService *service) {
-        [service put:query postBody:@"" callback:^(DOUHttpRequest *req) {
-            NSError *error = [req doubanError];
-            if (!error) {
-                [delegate performSelector:@selector(bookChangeRequestDidFinish)];
-            } else {
-                NSLog(@"error: %@", error);
-            }
-        }];
+        [service put:query postBody:@"" callback:callBackFunction];
     }];
 }
 
 - (void)addBook:(DOUBook *)book {
     [self doSomethingWithBook:book withBlock:^(DOUQuery *query, DOUService *service) {
-        [service post:query postBody:@"" callback:^(DOUHttpRequest *req) {
-            NSError *error = [req doubanError];
-            if (!error) {
-                [delegate performSelector:@selector(bookChangeRequestDidFinish)];
-            } else {
-                NSLog(@"error: %@", error);
-            }
-        }];
+        [service post:query postBody:@"" callback:callBackFunction];
     }];
+}
+
+- (void)initCallBackFunction {
+    callBackFunction = ^(DOUHttpRequest *req) {
+        NSError *error = [req doubanError];
+        if (!error) {
+            [delegate performSelector:@selector(bookChangeRequestDidFinish)];
+        } else {
+            NSLog(@"error: %@", error);
+        }
+    };
 }
 
 - (void)doSomethingWithBook:(DOUBook *)book
                   withBlock:(void (^)(DOUQuery *query, DOUService *service))block {
     NSString *subPath = [NSString stringWithFormat:@"/v2/book/%@/collection", [book id]];
-    DOUQuery *query = [[DOUQuery alloc] initWithSubPath:subPath parameters:@{@"status" : [book statusString], @"comment": [book myComment], @"rating":@""}];
+    DOUQuery *query = [[DOUQuery alloc] initWithSubPath:subPath parameters:@{@"status" : [book statusString], @"comment" : [book myComment], @"rating" : @""}];
     DOUService *service = [DOUService sharedInstance];
     block(query, service);
 }
