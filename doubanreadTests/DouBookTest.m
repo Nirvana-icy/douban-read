0b34836d00a24e46a6e5002a9b018b3d890905cb
jacky li <veryfaint@gmail.com>
2013-05-04 22:09:03 +0800

    add unit tests

--- doubanreadTests/DouBookTest.m
+++ doubanreadTests/DouBookTest.m
@@ -103,4 +103,23 @@
     assertThat([book myRatingStars], is(@[@2, @2, @2, @2, @2]));
 }
 
+- (void)test_should_get_0_when_my_rating_is_nil{
+    DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"rating" : @{}}];
+    assertThat([book myRating], is(@"0"));
+}
+
+- (void)test_should_update_my_rating{
+    NSMutableDictionary *ratingValue = [[NSMutableDictionary alloc]init];
+    ratingValue[@"value"] = @"1";
+    DOUBook *book = [[DOUBook alloc] initWithDictionary:@{@"rating" : ratingValue}];
+    [book setMyRating:2];
+    assertThat([book myRating], is(@"2"));
+}
+
+- (void)test_should_set_my_rating_when_my_rating_is_nil{
+    DOUBook *book = [[DOUBook alloc] initWithDictionary:@{}];
+    [book setMyRating:2];
+    assertThat([book myRating], is(@"2"));
+}
+
 @end
